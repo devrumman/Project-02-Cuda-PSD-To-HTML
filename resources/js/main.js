eed32a1f97f07488991d190d5da1e723d4a72325
Mohmudul Hassan Rumman <rummanahmed1991@gmail.com>
2021-03-23 19:18:10 +0600

    Full Done

--- resources/js/main.js
+++ resources/js/main.js
@@ -13,9 +13,9 @@ $(document).ready(function(){
 });
 
 function openNav() {
-    document.getElementById("myNav").style.width ="100%;"
+    document.getElementById("myNav").style.width ="100%";
 }
 
 function closeNav() {
-    document.getElementById("myNav").style.width ="0%;"
+    document.getElementById("myNav").style.width ="0%";
 }
