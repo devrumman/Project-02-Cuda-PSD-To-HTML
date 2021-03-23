8e5f6f726defbb1689d894934e5496f75aa8cd99
Mohmudul Hassan Rumman <rummanahmed1991@gmail.com>
2021-03-23 18:17:28 +0600

    3-23-2021

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,3 +1,21 @@
 $(document).ready(function(){
+    
+    $(".js--services-section").waypoint(function(direction){
+        if(direction == "down"){
+           $("nav").addClass("sticky");
+           } else{
+           $("nav").removeClass("sticky");
+           }
+    });
+    
+    //Mixitup Portfolio section\\
    var mixer = mixitup('.container'); 
 });
+
+function openNav() {
+    document.getElementById("myNav").style.width ="100%;"
+}
+
+function closeNav() {
+    document.getElementById("myNav").style.width ="0%;"
+}
